4ca45dce27e06500d08101800bfeffbb6e8eeefc
Gaby Rodrigues <127845196+gabzinh4@users.noreply.github.com>
2024-08-29 01:27:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const primeiroNome = "Gabriely";
+const sobrenome = "Souza"
+const idade = 18;
+const altura = 1.70;
+const massa = 55;
+const cidadeNatal = "São José dos Pinhais";
+
+
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidadeNatal);
